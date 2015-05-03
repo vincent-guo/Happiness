797d31a33af9198ba114dd7c05c9fc7cdca74e4e
Vincent Guo <vincent.weiguo@gmail.com>
2015-05-03 13:12:20 -0700

    Lecture 5: Objective-C Compatibility, Property List, Views

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Vincent Guo on 2015-05-02.
+//  Copyright (c) 2015 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Developing iOS 8 Apps with Swift: Happiness
+
+This is the happiness application for
+the [*Stanford University : Developing iOS 8 Apps with Swift*](https://itunes.apple.com/ca/course/developing-ios-8-apps-swift/id961180099)
+by [Vincent Guo](http://vincentguo.com/). 
